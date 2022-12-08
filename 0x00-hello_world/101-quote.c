c1a2401be1031c7731587a1ca793b540fa5db366
Chukwuma Osahor <osahorc@gmail.com>
2022-12-08 12:19:28 -0800

    m

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main -prints "Programming is like building a multilingual puzzle".
+ *
+ * Description: 'this program is described.'
+ *
+ * Return: Always 1.
+ */
+
+int main(void)
+{
+	write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
