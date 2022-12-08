6f00f717fef149dcfe1a358682395be5e798a8bf
Chukwuma Osahor <osahorc@gmail.com>
2022-12-08 12:40:49 -0800

    m

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 /**
  * main -prints "Programming is like building a multilingual puzzle".
  *
@@ -10,6 +11,6 @@
 
 int main(void)
 {
-	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
