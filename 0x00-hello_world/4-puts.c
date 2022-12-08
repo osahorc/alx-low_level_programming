3b62e2e442db356ab49f2dc4a517a7d029c3be0c
Chukwuma Osahor <osahorc@gmail.com>
2022-12-08 07:47:22 -0800

    m

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /*
  * main -prints "Programming is like building a multilingual puzzle".
- *
+ * description: this program is described
  * return: Always 0.
  */
-
-int main (void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 return (0);
