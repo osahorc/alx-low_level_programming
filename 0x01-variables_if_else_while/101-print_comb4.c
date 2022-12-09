aed5cc336982a113198cffc77044be967e3628d9
Chukwuma Osahor <osahorc@gmail.com>
2022-12-09 14:32:56 -0800

    m

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - this is the main function
+ *
+ * Description: -this is the dectiption.
+ * it has two lines
+ *
+ * Return: Always return 0.
+ */
+
+int main(void)
+
+{
+	int n;
+	int b;
+	int c;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		for (b = '0'; b <= '9'; b++)
+		{
+			for (c = '0'; c <= '9'; c++)
+			{
+			
+				putchar(n);
+				putchar(b);
+				if ((n == '9') && (b == '9'))
+					continue;
+				{
+					putchar (',');
+					putchar (' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+return (0);
+}
