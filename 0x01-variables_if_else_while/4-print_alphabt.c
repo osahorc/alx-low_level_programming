a845818ed9adc3c4899f5fd3b558fa1b6f7859d5
Chukwuma Osahor <osahorc@gmail.com>
2022-12-09 06:40:54 -0800

    m

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - this is the main function
+ *
+ * Description: -this is the dectiption.
+ * it has two lines
+ *
+ * Return: Always return 0.
+ */
+
+int main(void)
+
+{
+	char n;
+
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		if((n != 'q')) && ((n != 'e'))
+	{
+		putchar (n);
+	}
+	}
+	putchar('\n');
+return (0);
+}
