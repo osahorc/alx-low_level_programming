b5504534eec5492f0569e88054fd07be5acf0dfa
Chukwuma Osahor <osahorc@gmail.com>
2022-12-09 06:52:30 -0800

    m

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - this is the main function
+ *
+ * Description: -this is the dectiption.
+ * it has two lines
+ *
+ * Return: Always return 0.
+ */
+
+int main(void)
+
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		printf("%i", n);
+	}
+	putchar('\n');
+return (0);
+}
