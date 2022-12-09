9ac6bdb390575c5182d8004014b2c6f090688d32
Chukwuma Osahor <osahorc@gmail.com>
2022-12-09 07:25:17 -0800

    m

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - this is the main function
+ *
+ * Description: -this is the dectiption.
+ * it has two lines
+ *
+ * Return: Always return 0.
+ */
+
+int main(void)
+
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+		if ( n !='9')
+		{
+			putchar (',')
+			putchar (' ')
+		}
+	}
+	putchar('\n');
+return (0);
+}
