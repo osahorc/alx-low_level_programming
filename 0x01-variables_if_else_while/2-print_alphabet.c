bbeb207c91e05fafb7a33fcd11a8a37ca9aa2c45
Chukwuma Osahor <osahorc@gmail.com>
2022-12-09 05:56:35 -0800

    m

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - this is the main function
+ *
+ * Description: -this is the dectiption.
+ * it has two lines
+ *
+ * Return: Always return 0.
+ */
+
+int main(void)
+
+{
+	char n;
+	for (n = 'a'; n < 'z'; n++)
+		putchar (n);
+return (0);
+}
