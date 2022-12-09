ae3696d21c9006ca75af08c4ba0cb52a9e3540c2
Chukwuma Osahor <osahorc@gmail.com>
2022-12-09 06:59:33 -0800

    m

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - this is the main function
+ *
+ * Description: -this is the dectiption.
+ * it has two lines
+ *
+ * Return: Always return 0.
+ */
+
+int main(void)
+
+{
+	char n;
+
+	for (n = 'z'; n >= 'a'; n--)
+	{
+		putchar (n);
+	}
+	putchar('\n');
+return (0);
+}
