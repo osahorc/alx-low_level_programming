e5ea13293f8b8501f1151d7442794cb541de219d
Chukwuma Osahor <osahorc@gmail.com>
2022-12-13 12:14:56 -0800

    m

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include <unistd.h>
+/**
+ * main - this is the main function
+ *
+ * Description: -this is the dectiption.
+ * it has two lines
+ *
+ * Return: Always return 0.
+ */
+
+/* c is a character that will be printed to the screen */
+int _putchar(char c);
+void print_alphabet_x10(void);
+void print_alphabet_x10(void)
+
+{
+char c;
+int b;
+
+	for (b = 1; b <= 10; b++)
+	{
+		for (c ='a'; c <='z'; c++)
+			_putchar (c);
+	}
+	_putchar('\n');
+	return;
+}
