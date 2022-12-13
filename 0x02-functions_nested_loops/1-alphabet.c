662d27905cbd897f12bf89e73dc2eb402e57f359
Chukwuma Osahor <osahorc@gmail.com>
2022-12-13 11:30:02 -0800

    m

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,25 @@
+#include <unistd.h>
+/**
+ * main - this is the main function
+ *
+ * Description: -this is the dectiption.
+ * it has two lines
+ *
+ * Return: Always return 0.
+ */
+
+/* c is a character that will be printed to the screen */
+void print_alphabet(void);
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
