35a2247b65f68aaf57752fd9592f6268e8f49e1f
Chukwuma Osahor <osahorc@gmail.com>
2022-12-13 11:00:07 -0800

    m

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,3 @@
-#include "main.h"
-
 /**
  * main - this is the main function
  *
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _MAIN_H
+#define _MAIN_H
 #include <unistd.h>
 
 int _putchar(char c)
