0b1d2fd644a865e5bab6581d501784453d06c51e
Chukwuma Osahor <osahorc@gmail.com>
2022-12-14 10:43:18 -0800

    m

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints alphabets
+ * @n: number to cehck
+ * Return: 0 if lower
+ * -1 if otherwise
+ */
+void jack_bauer(void)
+{
+	int minute;
+	int hour;
+
+	for (hour = 0; hour <= 24; hour++)
+	{
+		for (minute = 0; minute <= 59; minute++)
+		{
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((minute / 10) + '0');
+			_putchar((minute % 10) + '0');
+			_putchar('\n');
+		}
+	}
