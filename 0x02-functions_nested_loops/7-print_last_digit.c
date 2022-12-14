6f36ce570f1db40171624a629581e2324e6cfa8f
Chukwuma Osahor <osahorc@gmail.com>
2022-12-14 09:57:42 -0800

    m

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints alphabets
+ * @n: number to cehck
+ * Return: 0 if lower
+ * -1 if otherwise
+ */
+int print_last_digit(int)
+{
+	if (n > 0)
+		n = n * -1
+			_putchar('0' + (n % 10));
+	return (n % 10);
+}
