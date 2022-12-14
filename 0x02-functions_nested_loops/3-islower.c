5cc91d6bc0a4ce49774425f35903a576a74a1cef
Chukwuma Osahor <osahorc@gmail.com>
2022-12-13 16:27:09 -0800

    m

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * islower - prints alphabets
+ * _islower - prints alphabets
  * @c: charter to cehck
- * Return: 0 if lower 
+ * Return: 0 if lower
  * -1 if otherwise
  */
 int _islower(int c)
