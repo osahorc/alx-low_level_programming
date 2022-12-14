e65ac6677da4b809ee6079aac16f095c91317947
Chukwuma Osahor <osahorc@gmail.com>
2022-12-14 05:17:24 -0800

    m

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_sign - prints alphabets
+ * @n: number to cehck
+ * Return: 0 if lower
+ * -1 if otherwise
+ */
+int _abs(int n)
+{
+	if (n > 0)
+	return (n);
+	
+	else
+	return (-n);
+	}
+}
