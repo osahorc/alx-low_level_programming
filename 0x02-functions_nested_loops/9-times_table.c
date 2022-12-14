ad9b200b2687e145b26f2bb6a6ff436615cbb70a
Chukwuma Osahor <osahorc@gmail.com>
2022-12-14 12:29:08 -0800

    m

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * times_table - prints alphabets
+ *
+ * Return: 0 if lower
+ * -1 if otherwise
+ */
+void times_table(void)
+{
+	int minute;
+	int hour;
+
+	for (hour = 0; hour <= 23; hour++)
+	{
+		for (minute = 0; minute <= 59; minute++)
+		{
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((minute / 10) + '0');
+			_putchar((minute % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
