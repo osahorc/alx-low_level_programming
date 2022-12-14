7c02afee6c89e72b55987c6cf1b0904270292ba3
Chukwuma Osahor <osahorc@gmail.com>
2022-12-14 00:52:46 -0800

    m

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_sign - prints alphabets
+ * @c: charter to cehck
+ * Return: 0 if lower
+ * -1 if otherwise
+ */
+int print_sign(int n)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
