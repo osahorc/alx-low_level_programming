eb4d44ffd12cacca2869fc770fc87afd4cfaecff
Chukwuma Osahor <osahorc@gmail.com>
2022-12-15 02:55:32 -0800

    m

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints alphabets
+ * 
+ * Return: result of multiplication
+ */
+void print_numbers(void)
+{
+	int a
+		for (a ='0'; a <= '9'; a++)
+		{
+			putchar(a);
+		}
+	putchar('\n');
+}
