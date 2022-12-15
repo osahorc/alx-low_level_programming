63a48c6bff584edec589c5cc28c5b27d56d278e6
Chukwuma Osahor <osahorc@gmail.com>
2022-12-15 11:19:25 -0800

    m

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,18 +2,7 @@
 #define MAIN_H
 
 int _putchar(char c);
-void print_alphabet_x10();
-void print_alphabet();
-int _islower(int c);
-int _isalpha(int c);
-void test_isalpha(int n);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
-void times_table(void);
-int _isupper(int c);
-int _isdigit(int c);
 int mul(int a, int b);
+int _isupper(int c);
 
 #endif /* MAIN_H */
