22988fcb27607eadb166fe9cfaaf4cd8adf895f5
Chukwuma Osahor <osahorc@gmail.com>
2022-12-15 11:15:04 -0800

    m

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,20 +1,19 @@
-#include "main.h"
+#include "holberton.h"
+
+
 
 /**
- * _islower - prints alphabets
- * @c: charter to cehck
- * Return: 0 if lower
- * -1 if otherwise
+ * _isupper - Checks for uppercase characters.
+ *
+ * @c: The character to be checked.
+ *
+ * Return: 1 if character is uppercase, 0 otherwise.
  */
-int _isrrr(int c)
+
+int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
-	{
 		return (1);
-	}
 	else
-	{
 		return (0);
-	}
-	_putchar('\n');
 }
