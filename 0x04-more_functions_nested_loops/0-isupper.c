0208e63c954675d300da3acef58cac3afd212c98
Chukwuma Osahor <osahorc@gmail.com>
2022-12-14 23:53:23 -0800

    m

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _islower - prints alphabets
+ * @c: charter to cehck
+ * Return: 0 if lower
+ * -1 if otherwise
+ */
+int _isrrr(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
---
+++ 0x04-more_functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _islower - prints alphabets
+ * @c: charter to cehck
+ * Return: 0 if lower
+ * -1 if otherwise
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
