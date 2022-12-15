5600ba3e914abfd8bc458c52446ffd94659bbd37
Chukwuma Osahor <osahorc@gmail.com>
2022-12-15 11:50:02 -0800

    m

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,16 +1,19 @@
 #include "main.h"
 
 /**
- * print_numbers - prints alphabets
- * 
+ * print_numbers - multipies figures
+ * charter to cehck
+ * second carachter to multiply
  * Return: result of multiplication
  */
 void print_numbers(void)
+
 {
-	int a
-		for (a ='0'; a <= '9'; a++)
-		{
-			putchar(a);
-		}
-	putchar('\n');
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		_putchar('0' + i);
+	}
+	_putchar('\n');
 }
