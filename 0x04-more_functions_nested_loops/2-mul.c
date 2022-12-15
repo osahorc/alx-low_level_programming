aa36fa5ea5b2e3ba05adedba5563ea472374f25c
Chukwuma Osahor <osahorc@gmail.com>
2022-12-15 06:17:32 -0800

    m

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,11 +1,13 @@
 #include "main.h"
 
 /**
- * _islower - prints alphabets
+ * mul - multipies figures
  * @a: charter to cehck
  * @b: second carachter to multiply
+ *
  * Return: result of multiplication
  */
+
 int mul(int a, int b)
 {
 	return(a * b);
