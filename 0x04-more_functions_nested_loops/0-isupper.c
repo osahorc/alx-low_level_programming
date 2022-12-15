d2fe682a74a130175aa9d2554184054a7e3a65bc
Chukwuma Osahor <osahorc@gmail.com>
2022-12-15 11:33:07 -0800

    m

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,15 +1,13 @@
 #include "main.h"
 #include <ctype.h>
-/**
- * _isupper - checks if it is upper
- *
- * @c: is the character
- *
- * Return: returns 1 if upper
- * otherwise 0 is returned
- */
 
+/**
+ * _isupper - multipies figures
+ * @c: charter to cehck
+ * Return: result of multiplication
+ */
 int _isupper(int c)
+
 {
-	return isupper (c);
+	return isupper(c);
 }
