e6aeb7b3b02b8209f78642a463e7c8aa9680a12e
Chukwuma Osahor <osahorc@gmail.com>
2022-12-16 10:06:30 -0800

    m

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -13,10 +13,10 @@ void print_diagonal(int n)
 	int a;
 	if (n > 0)
 	{
-	for (a = 0; a < n; a++)
-	{
-		_putchar('\');
-	}
+		for (a = 0; a < n; a++)
+		{
+			_putchar(92);
+		}
 	}
 	_putchar('\n');
 }
