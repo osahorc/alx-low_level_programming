d2b57fabef9a14823ef35b1d8e9afb61905f2863
Chukwuma Osahor <osahorc@gmail.com>
2022-12-16 09:59:28 -0800

    m

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints a line
+ *
+ * @n: the number of times the line will primnt
+ *
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+	int a;
+	if (n > 0)
+	{
+	for (a = 0; a < n; a++)
+	{
+		_putchar('\');
+	}
+	}
+	_putchar('\n');
+}
