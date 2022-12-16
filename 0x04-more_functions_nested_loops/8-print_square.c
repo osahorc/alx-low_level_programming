6fbcc8e561f21c8d1a9fcd2ad6c1dd4f3c3112d5
Chukwuma Osahor <osahorc@gmail.com>
2022-12-16 11:18:54 -0800

    m

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -12,14 +12,16 @@ void print_square(int size)
 {
 	int a;
 	int b;
-
-	for (a = 0; a < size; a++)
+	if (size > 0)
 	{
-		for (b = 0; b < size; b++)
+		for (a = 0; a < size; a++)
 		{
-			_putchar('#');
+			for (b = 0; b < size; b++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
 		}
 		_putchar('\n');
 	}
-	_putchar('\n');
 }
