04f6e3857d91fec7d35ff59ac0eb79dd43062059
Chukwuma Osahor <osahorc@gmail.com>
2022-12-16 07:23:02 -0800

    m

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,20 +1,20 @@
 #include "main.h"
 
 /**
- * print_numbers - multipies figures
- * charter to cehck
- * second carachter to multiply
+ * print_numbers - prints the numbers 0 to 9 except 2 and 4, and then prints a new line
+ * 
  * Return: void
  */
 void print_numbers(void)
-
 {
 	int i;
 
 	for (i = 1; i <= 9; i++)
 	{
 		if (i != 2 && i != 4)
+		{
 		_putchar(48 + i);
+		}
 	}
 	_putchar('\n');
 }
