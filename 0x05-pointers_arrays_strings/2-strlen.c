54da7fb7410680f262a966088532d590ebc570cc
Chukwuma Osahor <osahorc@gmail.com>
2022-12-19 08:25:29 -0800

    m

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - resets a pointer to 98
+ *
+ * @s: The value of the pointed
+ *
+ * Return: void
+ */
+
+int _strlen(char *s);
+
+{
+	int count;
+	for (count = 0; s[count] != '\0'; count++);
+		return count;	
+}
