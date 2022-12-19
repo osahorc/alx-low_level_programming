e8b818d85e8c9b546732d6ea1f15d5166a45f797
Chukwuma Osahor <osahorc@gmail.com>
2022-12-19 12:20:56 -0800

    m

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts - resets a pointer to 98
+ *
+ * @s: The value of the pointed
+ *
+ * Return: void
+ */
+
+void _puts(char *str)
+
+{
+	char count;
+
+	for (count = 0; s[count] != '\0'; count++)
+	{
+		_puts(*str);
+		_puts('\n');
+	}
+}
