7f095d053cb8ee0c2443670036048a378755aabf
Chukwuma Osahor <osahorc@gmail.com>
2022-12-20 11:21:59 -0800

    m

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * void print_array - resets a pointer to 98
+ *
+ * @*a: The value of the pointed
+ * @n: value of the array
+ *
+ * Return: void
+ */
+
+void print_array(int *a, int n)
+
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i < n -1)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n")
+}
