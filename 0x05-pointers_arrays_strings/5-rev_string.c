ea2942b05f3dd45fa410ac29940dce4f67e89141
Chukwuma Osahor <osahorc@gmail.com>
2022-12-20 02:33:51 -0800

    m

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * rev_string - resets a pointer to 98
+ *
+ * @s: The value of the pointed
+ *
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	int len = strlen(s);
+	int count;
+
+	for (count = 0; count = len/2; count--)
+	{
+		char temp = s[count];
+		s[count] = [len - count - 1];
+		[len - count - 1] = temp;
+	}
+}
