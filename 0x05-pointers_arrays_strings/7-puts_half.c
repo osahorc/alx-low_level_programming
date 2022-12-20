cb910459122b8cda50c16b684e96e572cdb5c28c
Chukwuma Osahor <osahorc@gmail.com>
2022-12-20 06:42:49 -0800

    m

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * puts_half - resets a pointer to 98
+ *
+ * @str: The value of the pointed
+ *
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int l
+	for (len = 0; str[len] != '\n'; len++)
+	{
+	}
+	if (len % 2 == 0)
+	{
+		start = len / 2;
+	}
+	else
+	{
+		start = (len - 1) / 2;
+	} 
+}
