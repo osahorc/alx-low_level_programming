d9931f104a70070199c3899fad0bb0c2c37bf331
Chukwuma Osahor <osahorc@gmail.com>
2022-12-20 01:46:27 -0800

    m

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev - resets a pointer to 98
+ *
+ * @s: The value of the pointed
+ *
+ * Return: void
+ */
+
+void print_rev(char *s)
+
+{
+	len = _strlen(s);
+	int count;
+
+	for (count = len -1; s[count] >= 0'; count--)
+	{
+		_putchar(str[count]);
+	}
+		_putchar('\n');
+}
