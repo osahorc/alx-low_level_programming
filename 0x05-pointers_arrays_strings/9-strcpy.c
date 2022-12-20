56994e5996a10c62c355020091bef95d97eb117a
Chukwuma Osahor <osahorc@gmail.com>
2022-12-20 11:52:04 -0800

    m

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_strcpy - resets a pointer to 98
+ *
+ * @dest: The value of the pointed
+ * @src: value of the array
+ *
+ * Return: dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+	return dest;
+}
