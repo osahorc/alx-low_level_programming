b8b51fe9b3178b3f9cc1c8cd384665cf13bdc6e4
Chukwuma Osahor <osahorc@gmail.com>
2022-12-20 01:31:56 -0800

    m

---
+++ 0x05-pointers_arrays_strings/0-putchar.c
@@ -0,0 +1,25 @@
+#include <unistd.h>
+/**
+ * main - this is the main function
+ *
+ * @c:his is the dectiption.
+ * it has two lines
+ *
+ * Return: Always return 0.
+ */
+
+/* c is a character that will be printed to the screen */
+int _putchar(char c);
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
