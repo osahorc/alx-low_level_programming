e707cabe43900bd022c54a9904e83dde99992b00
Chukwuma Osahor <osahorc@gmail.com>
2022-12-23 13:49:29 -0800

    m

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -16,7 +16,7 @@ char *_strcat(char *dest, char *src)
 	int j;
 	for ( i = 0; dest[i] != '\0'; i++);
 
-	for (j = 0; scr[j] != '\0'; j++, i++)
+	for (j = 0; src[j] != '\0'; j++, i++)
 	{
 		dest[i] = src[j];
 	}
