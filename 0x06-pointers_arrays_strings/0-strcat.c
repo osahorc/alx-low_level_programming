1299c95479d68f23dc88a22e2f3fa9c39e562099
Chukwuma Osahor <osahorc@gmail.com>
2022-12-23 13:47:56 -0800

    m

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -13,9 +13,10 @@
 char *_strcat(char *dest, char *src)
 {
 	int i;
+	int j;
 	for ( i = 0; dest[i] != '\0'; i++);
 
-	for (int j = 0; scr[j] != '\0'; j++, i++)
+	for (j = 0; scr[j] != '\0'; j++, i++)
 	{
 		dest[i] = src[j];
 	}
