f56fb98195d5eeab1add09e7ef6f7a7fb620bd16
Chukwuma Osahor <osahorc@gmail.com>
2023-01-09 03:30:33 -0800

    m

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - takes two arguments
+ * @argc - Argument
+ * @argv - Argument
+ * return: 0
+ */
+
+int main(int argc, char* argv[])
+{
+	printf ("%d", argv[0]);
+		return: 0;
+}
